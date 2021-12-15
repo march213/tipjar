94ca1e5ec82eb75bf7205d99873817f0f09b40a3
Jane Molodetskaya <jane.molodetskaya@gmail.com>
2021-12-14 21:28:48 -0500

    show tip form only if a wallet connected

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 import Web3 from 'web3/dist/web3.min.js'
 
 const web3 = new Web3(Web3.givenProvider || 'ws://localhost:8545')
+const form = document.querySelector('form')
+
+if (window.ethereum) {
+  form.classList.add('has-eth')
+}
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault()
+  const tipValue = event.target.elements.tip.value
+})
